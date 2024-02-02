bfb515a0a53b5e1558ce9ea9328b3ee2c108bcb9
Your Name <you@example.com>
2024-02-02 15:32:34 +0500

    second

---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+body: ListView.builder(
+  itemCount: 10,
+  itemBuilder: (ctx, index)=> Container(
+  padding: EdgeInsets.all(10),
+  child: Text(''),
+)),
+    );
+  }
+}
